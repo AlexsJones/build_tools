0d87d9cf76d22bd96163d087f143ffe1275d8639
jonesax <jonesax@devml0253912.bskyb.com>
2017-01-12 12:18:37 +0000

    basic working

---
+++ app.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+##########################################################################
+#     File Name           :     app.py
+#     Created By          :     jonesax
+#     Creation Date       :     [2017-01-12 11:49]
+#     Last Modified       :     [2017-01-12 12:11]
+#     Description         :
+##########################################################################
+
+from flask import Flask, Response,render_template
+import os
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, static_folder='public', template_folder=tmpl_dir)
+
+@app.route("/")
+def index():
+  return render_template('layout.html')
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
