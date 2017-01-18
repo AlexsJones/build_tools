b52c54446deee2f54bc5063e6cb67cea6fc5bf4b
Alex Jones <tibbar@freedommail.ch>
2017-01-18 10:50:18 +0000

    working

--- app.py
+++ app.py
@@ -9,16 +9,28 @@
 
 from flask import Flask, Response,render_template
 import os
+from build_tools.services.gitlab_service import gitlab_service
+
+
+class options():
+    command = "print_stats"
+    gitlab_server = "http://gitlab.intranet.sky"
+    gitlab_token = "QR18DoufKscQAF6HA_BD"
+    gitlab_project = "ce-devices-ios/Benji"
+    gitlab_max_size = 10
+
+gs = gitlab_service()
+
+merge, user_info = gs.run(options)
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, static_folder='public', template_folder=tmpl_dir)
 
+
 @app.route("/")
 def index():
-  return render_template('homepage.html', content = 'hi')
-
-
+    return render_template('homepage.html', user_info=user_info)
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True,port=2001)
