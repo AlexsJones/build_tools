5f2e4b2f96a6a3ebb9af668ce6b53470a4d3bf93
Marsh, Edward <edward.marsh@sky.uk>
2017-01-30 16:35:56 +0000

    Add Page status json

--- src/socket_merge_request_bindings.py
+++ src/socket_merge_request_bindings.py
@@ -41,7 +41,44 @@ class MergeRequestSocketNameSpace(Namespace):
         # emit('populate_updates',
         #      {'total_open': to, 'total_closed': tc, 'total_wip': tw})
 
-        pass
+
+            start_date = parse_string(request.form['start_date'])
+            end_date = parse_string(request.form['end_date'])
+            options.gitlab_stats_start_date = start_date
+            options.gitlab_stats_end_date = end_date
+            error = None
+            merge, user_info = gs.run(options)
+
+            start_date = datetime.strptime(start_date, '%d/%m/%Y')
+            if start_date > datetime.now():
+                error = "The Start date cannot be in the future."
+
+
+            end_date = datetime.strptime(end_date, '%d/%m/%Y')
+
+            if start_date > end_date:
+                error = "The end date cannot be before the start date."
+
+            filtered_merges = []
+
+            for x in merge:
+                merge_date = self.gs.parse_datetime(x.created_at)
+                if start_date <= merge_date <= end_date:
+                    filtered_merges.append(x)
+            start_date = start_date.strftime('%d/%m/%Y')
+            end_date = end_date.strftime('%d/%m/%Y')
+
+            if len(filtered_merges) == 0 and error is None:
+                page_status = "No requests were made between " + start_date + " and " + end_date
+                emit('filter_requests', {'page_status': page_status})
+            elif error is not None:
+                page_status = ""
+                emit('filter_requests', {'page_status': page_status})
+            else:
+                page_status = "Currently showing requests between " + start_date + " and " + end_date
+                emit('filter_requests', {'page_status': page_status})
+
+
 
     @staticmethod
     def fill_table(self, message):
