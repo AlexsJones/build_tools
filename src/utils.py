369c343a43f2ffd3b53e5f96344d15ad372080d3
ed11e <ed11e@users.noreply.github.com>
2017-02-02 15:52:28 +0000

    Revert "Thread reset"

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 
 def parse_string(d):
     s = d.split('-')
@@ -10,10 +9,3 @@ def toJson(dict):
     jd = json.dumps(dict)
     return jd
 
-def get_first_day():
-    today = datetime.today()
-    today = today.strftime('%d/%m/%Y')
-    today = today[2:]
-    first_day_s = "01" + today
-    first_day = datetime.strptime(first_day_s, "%d/%m/%Y")
-    return first_day
