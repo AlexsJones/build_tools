e26353b4a420b273546ee027135135c52b7f0f6b
AlexsJones <tibbar@freedommail.ch>
2017-02-10 13:32:51 +0000

    updated

--- src/bindings.py
+++
@@ -1,8 +0,0 @@
-from flask_socketio import Namespace, emit
-
-
-class Bindings(Namespace):
-    def __init__(self, service, namespace=None):
-        super(Namespace, self).__init__(namespace)
-        self.gs = service
-        self.results_store = dict([])
